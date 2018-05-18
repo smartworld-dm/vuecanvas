dd752ae98aab51f8ceb266593eeeb36a8130d9ab
smartworld1004 <smartworld1004@outlook.com>
2018-05-18 23:43:08 +0800

    feat: dashboard - completed exclude gesture

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,6 +1,24 @@
 <template lang="html">
   <div>
-    <button v-on:click="onClick()" style="width: 150px; height: 40px; margin-bottom: 20px;">Start</button>
+    <div class="header">
+      <p class="name">Ramin Tahbaz-Salehi</p>
+      <p class="all-product">view all projects</p>
+    </div>
+    <div class="arrows">
+      <img src="public/images/arrow-up-12.svg" class="arrow-up-12" v-on:click="onClickArrow(1)">
+      <img src="public/images/arrow-down-12.svg" class="arrow-down-12" v-on:click="onClickArrow(2)">
+    </div>
+    <div class="indicator">
+      <indicator v-for="n in 5" :sceneindex="sceneIndex" :index="n - 1" :title="indicators[n - 1]"></indicator>
+    </div>
+    <div class="scene-description">
+      <p>{{ sceneDescription.title[sceneIndex] }}</p>
+      <p>{{ sceneDescription.desc[sceneIndex] }}</p>
+    </div>
+    <div class="link">
+      <img src="public/images/email-send-1.svg" class="email-send-1">
+      <img src="public/images/logo-dribbble.svg" class="logo-dribble">
+    </div>
     <div id="cvs">
     </div>
   </div>
@@ -8,18 +26,37 @@
 <script>
 import {Container, WebGLRenderer, Sprite, Graphics, Filter, Matrix} from './../assets/javascripts/pixi.min.js'
 
+import { mapState } from 'vuex';
+
+import Indicator from './Indicator.vue';
+
 export default {
   props: {
   },
+  components: {
+    Indicator,
+  },
   data: () => {
     return {
-      mainImgID: 0,
+      // Static strings
+      indicators: ['welcome', 'Hi', 'Good', 'Working', 'Design'],
+      sceneDescription: {
+        title: ['Hello everyone!', 'Hi, you know?', 'Really impressive!', 'Gotcha!', 'Soon!!!'],
+        desc: ['I’m an American UX/UI designer in Washington, DC currently working at Freddie Mac.',
+                '2nd scene description',
+                '3nd scene description',
+                '4nd scene description',
+                '5nd scene description',
+              ]
+      },
+      // Screen
+      width: 0,
+      height: 0,
       // Container
       container: null,
       stage: null,
       // Mask
       bg: null,
-      bg1: null,
       mask: null,
       maskScale: 0,
       // Renderer
@@ -31,26 +68,62 @@ export default {
       // Update?
       isUpdate: false,
       // Frame
-      fps: 20,
+      fps: 10,
       now: 0,
       then: 0,
       interval: 0,
       delta: 0,
     }
   },
+  updated (){
+    // console.log("aaa - ", this.$store.state.sceneIndex)
+    this.onReload()
+  },
   mounted: function () {
+    // Add Listener
+    window.addEventListener('resize', this.onResize)
+
+    // Screen size
+    this.width = window.innerWidth
+    this.height = window.innerHeight
+
+    // Create PIXI objects
     this.container = new PIXI.Container()
     this.stage = new PIXI.Container()
-    this.renderer = new PIXI.autoDetectRenderer(800, 600, {antialias: true} )
+    this.renderer = new PIXI.autoDetectRenderer(this.width, this.height, {antialias: true})
     document.getElementById("cvs").appendChild(this.renderer.view)
 
     // Frame
-    this.interval = 1000 / this.fps
+    this.interval = 1001 / this.fps
     this.then = Date.now()
     
-    this.drawCanvas()
+    this.onReload()
   },
   methods: {
+    onClickArrow: function(direction){
+      var index = this.$store.state.sceneIndex
+      if (direction === 1) index--
+      else if (direction === 2) index++
+
+      index = index === 5 ? 0 : index
+      index = index === -1 ? 4 : index
+      this.$store.commit('updateSceneIndex', index)
+    },
+    onReload: function (){
+      this.width = window.innerWidth
+      this.height = window.innerHeight
+      this.container.removeChild(this.bg)
+      this.drawBackground(this.$store.state.sceneIndex)
+      this.onClick()
+    },
+    onResize(event) {
+      this.width = window.innerWidth
+      this.height = window.innerHeight
+      this.renderer.view.style.width = this.width + 'px'
+      this.renderer.view.style.height = this.height + 'px'
+      this.container.removeChild(this.bg)
+      this.drawBackground(this.$store.state.sceneIndex)
+    },
     onClick: function (){
       this.counter = 0
       this.isUpdate = true
@@ -59,14 +132,14 @@ export default {
       this.container.mask = null;
 
       this.mask = PIXI.Sprite.fromImage('public/images/cloud-texture4.png')
-      this.mask.anchor.x = 0.5
-      this.mask.anchor.y = 0.5
-      this.mask.position.x = this.renderer.width / 2
-      this.mask.position.y = this.renderer.height / 2
+      this.mask.anchor.x = .5 
+      this.mask.anchor.y = .5
+      this.mask.position.x = this.renderer.width / 2 + (100 - Math.floor(Math.random() * 200))
+      this.mask.position.y = this.renderer.height / 2 + (100 - Math.floor(Math.random() * 200))
       this.mask.width = 100
       this.mask.height = 100
       this.mask.alpha = 0.9
-      this.mask.rotation = Math.floor(Math.random() * 11) 
+      this.mask.rotation = Math.floor(Math.random() * 11)
 
       this.stage.addChild(this.mask)
       this.container.mask = this.mask
@@ -74,7 +147,7 @@ export default {
       this.animate()
     },
     animate: function (){
-      if (this.counter > 20){
+      if (this.counter > 10){
       console.log("counter - ", this.counter);
         this.isUpdate = false
         this.stage.removeChild(this.mask);
@@ -89,28 +162,31 @@ export default {
       if (this.delta > this.interval) {
         this.then = this.now - (this.delta % this.interval)
 
-        this.mask.width += this.counter++ * 20
-        this.mask.height += this.counter * 20
+        this.mask.width += this.counter++ * 50
+        this.mask.height += this.counter * 50
         this.renderer.render(this.stage)
       }
     },
-    drawCanvas: function() {
+    drawBackground: function(bgIndex) {
       this.stage.interactive = true
 
       this.container.position.x = this.renderer.width / 2
       this.container.position.y = this.renderer.height / 2
-      this.bg1 = PIXI.Sprite.fromImage('public/images/first.jpg')
-      this.bg1.anchor.x = 0.5
-      this.bg1.anchor.y = 0.5
-      this.bg1.position.x = 0
-      this.bg1.position.y = 0
-      this.bg1.width = 800
-      this.bg1.height = 600
+      this.bg = PIXI.Sprite.fromImage('public/images/' + bgIndex + '.jpg')
+      this.bg.anchor.x = .5
+      this.bg.anchor.y = .5
+      this.bg.position.x = 0
+      this.bg.position.y = 0
+      this.bg.width = this.width
+      this.bg.height = this.height
       
-      this.container.addChild(this.bg1)
+      this.container.addChild(this.bg)
       this.stage.addChild(this.container)
       this.renderer.render(this.stage)
     }
-  }
+  },
+  computed: mapState({
+    sceneIndex: state => state.sceneIndex
+  }),
 }
 </script>
